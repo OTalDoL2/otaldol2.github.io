0cde4da12cf114a081cc1cc0ba15af4e7bb0ceaf
L2 <lucas.ramos_dev@outlook.com>
2021-01-18 15:04:04 -0300

    Projeto (pc - 95% / Mobile - 50%)

--- JavaScript/slideColab.js
+++ JavaScript/slideColab.js
@@ -1,12 +1,3 @@
-// $(document).ready(function() {
-//     $('#autoWidth').lightSlider({
-//         autoWidth:true,
-//         loop:true,
-//         onSliderLoad: function() {
-//             $('#autoWidth').removeClass('cS-hidden');
-//         } 
-//     });  
-//   });
 
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 3,
